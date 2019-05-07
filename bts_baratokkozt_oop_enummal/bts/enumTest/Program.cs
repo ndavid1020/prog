d8122e0d306cda273aa9805f25b77b822610e5d8
ndavid1020 <47393761+ndavid1020@users.noreply.github.com>
2019-05-07 19:08:48 +0200

    Backtrack minta (Ez van a doksiban)

---
+++ bts_baratokkozt_oop_enummal/bts/enumTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumTest
+{
+    class Program
+    {
+        public enum Napok
+        {
+            Hétfő       = 1,
+            Kedd        = 2,
+            Szerda      = 3,
+            Csütörtök   = 4,
+            Péntek      = 5,
+            Szombat     = 6,
+            Vasárnap   // = 7 alapból indexel
+        }
+
+        public enum Italok
+        {
+            Tonic = 990,
+            Cola = 120,
+            JimBeam = 9990
+        }
+
+        static void Main(string[] args)
+        {   
+        }
+    }
+}
